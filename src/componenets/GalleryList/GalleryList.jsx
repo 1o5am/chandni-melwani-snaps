48cc56c7dc5e80a6e7081a0e80aecb199860d857
Chandni Melwani <cmelwani92@gmail.com>
2025-01-19 23:35:13 -0500

    added functionality to see photos based on filter

--- src/componenets/GalleryList/GalleryList.jsx
+++ src/componenets/GalleryList/GalleryList.jsx
@@ -1,7 +1,16 @@
 import photos from "../../data/photos.json"
 import GalleryCard from "../GalleryCard/GalleryCard"
 
-export default function() {
+export default function GalleryList({activeFilter}) {
+
+    // Functionality to show list if filter is not clicked on. If clicked on show list of photos from tag. If not clicked on or clicked on tag again, show all photos.
+    let filteredPhotos = photos.filter((photo) => {
+        if (activeFilter === null) {
+            return true;
+        } else if (photo.tag === activeFilter) {
+            return true;
+        }
+    });
 
     return (
         <div className="gallery-list">
