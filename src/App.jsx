ddd7fdf65c186126cb6ba5fbbf7b601f4693476b
Chandni Melwani <cmelwani92@gmail.com>
2025-01-19 21:21:08 -0500

    added hero section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.scss'
 import Header from "./componenets/Header/Header";
 import Main from "./componenets/Main/Main"
+import Hero from "./componenets/Hero/Hero"
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>
     <div className="wholePage">
       <Header />
+      <Hero />
       <Main />
       </div>
     </>
