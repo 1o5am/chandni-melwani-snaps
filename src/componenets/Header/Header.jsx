47c2b8d10fed23a78c5531ebb74cd5ffb5b56aaa
Chandni Melwani <cmelwani92@gmail.com>
2025-01-19 23:50:18 -0500

    switched div on header__filter-button to button

--- src/componenets/Header/Header.jsx
+++ src/componenets/Header/Header.jsx
@@ -6,13 +6,13 @@ function Header ({ filterOpen, setFilterOpen }) {
     return (
         <header className="header">
             <span className="header__title">Snaps</span>
-            <div 
+            <button 
                 className={`header__filter-button ${filterOpen ? "header__filter-button--active" : ""}`}
                 onClick={() => setFilterOpen((prev) => !prev)}
             >
                 <span className="header__filter">Filters</span>
                 <img src={filterIcon} alt="filter menu"/>
-            </div>
+            </button>
         </header>
     );
 }
