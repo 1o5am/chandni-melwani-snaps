8745c07ea8f1752b490e7c19409f7975964cb072
Chandni Melwani <cmelwani92@gmail.com>
2025-01-19 23:16:50 -0500

    added filter Open state conditions

--- src/componenets/Header/Header.jsx
+++ src/componenets/Header/Header.jsx
@@ -2,11 +2,14 @@ import './Header.scss'
 import filterIcon from '../../assets/images/Filter.svg'
 import Filter from '../Filter/Filter'
 
-function Header () {
+function Header ({ filterOpen, setFilterOpen }) {
     return (
         <header className="header">
             <span className="header__title">Snaps</span>
-            <div className="header__filter-button">
+            <div 
+                className={`header__filter-button ${filterOpen ? "header__filter-button--active" : ""}`}
+                onClick={() => setFilterOpen((prev) => !prev)}
+            >
                 <span className="header__filter">Filters</span>
                 <img src={filterIcon} alt="filter menu"/>
             </div>
