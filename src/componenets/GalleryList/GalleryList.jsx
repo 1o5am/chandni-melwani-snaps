0f9275e4521acd400e4749dd1ef2ec5e83de0007
Chandni Melwani <cmelwani92@gmail.com>
2025-01-20 02:01:05 -0500

    updated filter photo fx

--- src/componenets/GalleryList/GalleryList.jsx
+++ src/componenets/GalleryList/GalleryList.jsx
@@ -2,14 +2,23 @@ import photos from "../../data/photos.json"
 import GalleryCard from "../GalleryCard/GalleryCard"
 import "./GalleryList.scss"
 
-export default function() {
+export default function( {activeFilter} ) {
+
+    // Filter photos based on the active filter
+    const filteredPhotos = photos.filter((photo) => {
+        // Return all photos if no filter is active
+        if (!activeFilter) {
+            return true;
+        }
+        // Check if the photo's tags array includes the activeFilter
+        return photo.tags.includes(activeFilter);
+    });
+
 
     return (
         <div className="gallery-list">
-            {photos.map((photo) => {
-                return (
-                    <GalleryCard key={photo.id} photo={photo} />
-                );
+            {filteredPhotos.map((photo) => {
+                return <GalleryCard key={photo.id} photo={photo} />;
             }
         )}
         </div>
