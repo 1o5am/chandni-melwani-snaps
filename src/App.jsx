4b5161a40b1333cb9ed028025f6ae02a53de8ba5
Chandni Melwani <cmelwani92@gmail.com>
2025-01-19 21:09:29 -0500

    removed prev content

--- src/App.jsx
+++ src/App.jsx
@@ -10,41 +10,6 @@ function App() {
     <div className="wholePage">
       <Header />
       <Main />
-
-
-      <section className="hero">
-        <p className="hero__short-text">Our mission:</p>
-        <p className="hero__long-text">Provide photographers a space to share photos of the neighborhoods they cherish,<span className='hero__long-text__italic'> expressed in their unique style</span>
-        </p>
-      </section>
-      
-      <main className="gallery">
-        <ul>
-          <li className='gallery__item'>
-            Image1
-          </li>
-          <li className='gallery__item'>
-            Image2
-          </li>
-          <li className='gallery__item'>
-            Image3
-          </li>
-          <li className='gallery__item'>
-            Image4
-          </li>
-        </ul>
-        <section>
-          Main Content
-        </section>
-      </main>
-
-      <aside>
-        Aside Content
-      </aside>
-
-      <footer>
-        Footer Content
-      </footer>
       </div>
     </>
   );
