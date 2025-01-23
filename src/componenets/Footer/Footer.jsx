03e5953bd8a434e6589e3df086222b1a4710cdc9
Chandni Melwani <cmelwani92@gmail.com>
2025-01-22 21:00:19 -0500

    modified footer

--- src/componenets/Footer/Footer.jsx
+++ src/componenets/Footer/Footer.jsx
@@ -8,31 +8,50 @@ import pinterestIcon from '../..//assets/images/Pinterest.svg';
 export default function Footer() {
     return (
         <footer className="footer">
-            <div className="footer__content">
-                <div className="footer__title">Snaps</div>
-                <div className="footer__links-one">
-                    <div className="footer__link">For Photographers</div>
-                    <div className="footer__link">Hire Talent</div>
-                    <div className="footer__link">Inspiration</div>
+            <div className="footer__title-content-social-media">
+                <div className="footer__title-content">
+                    <div className="footer__title">
+                        Snaps
+                    </div>
+                        <div className="footer__content">
+                            <div className="footer__content-one">
+                                <p className="footer__link">
+                                    For Photographers
+                                </p>
+                                <p className="footer__link">
+                                    Hire Talent
+                                </p>
+                                <p className="footer__link">
+                                    Inspiration
+                                </p>
+                            </div>
+                            <div className="footer__content-two">
+                                <p className="footer__link">
+                                    About
+                                </p>
+                                <p className="footer__link">
+                                    Careers
+                                </p>
+                                <p className="footer__link">
+                                    Support
+                                </p>
+                            </div>
+                        </div>
                 </div>
-                <div className="footer__links-two">
-                    <div className="footer__link">About</div>
-                    <div className="footer__link">Careers</div>
-                    <div className="footer__link">Support</div>
-                </div>
-                <div className="footer__social-media">
-                    <img src={facebookIcon} alt="facebook icon" />
-                    <img src={instagramIcon} alt="instagram icon" />
-                    <img src={XIcon} alt="X icon" />
-                    <img src={pinterestIcon} alt="pinterest icon" />
-                </div>
-                <div className="footer__copyright">
+                    <div className="footer__social-media">
+                        <img src={facebookIcon} alt="facebook icon" />
+                        <img src={instagramIcon} alt="instagram icon" />
+                        <img src={XIcon} alt="X icon" />
+                        <img src={pinterestIcon} alt="pinterest icon" />
+                    </div>
+            </div>
+            <div className="footer__copyright">
                     © 2024 Snaps
                     <div className="footer__link">Terms</div>
                     <div className="footer__link">Privacy</div>
                     <div className="footer__link">Cookies</div>
-                </div>
             </div>
+
         </footer>
     );
 }
