978c75e5317078eb0a7639108b94845f651ef6ef
Chandni Melwani <cmelwani92@gmail.com>
2025-01-26 18:26:44 -0500

    addedcomments through API and created functionality to render new comment on top of comment from api

--- src/componenets/pages/PhotoPage/PhotoPage.jsx
+++ src/componenets/pages/PhotoPage/PhotoPage.jsx
@@ -25,13 +25,51 @@ export default function PhotoPage() {
         fetchPhoto(photoId);
     }, [photoId]);
 
+
+    const [comments, setComments] = useState([]);
+
+    const fetchComments = async (id) => {
+        try {
+            const response = await axios.get(`https://unit-3-project-c5faaab51857.herokuapp.com/photos/${id}/comments?api_key=${API_KEY}`);
+            const sortedComments = response.data.sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
+            console.log(response.data);
+            setComments(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchComments(photoId);
+    }, [photoId]);
+
+
+    const [newComment, setNewComment] = useState({ name: '', comment: '' });
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setNewComment({ ...newComment, [name]: value });
+    };
+
+    const handleCommentSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post(`https://unit-3-project-c5faaab51857.herokuapp.com/photos/${photoId}/comments?api_key=${API_KEY}`, newComment);
+            console.log(response.data);
+            setComments([response.data, ...comments]);
+            setNewComment({ name: '', comment: '' });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return(
         <main className="photo-page">
             <div className="photo__container">
                 {photo && (
                     <div>
-                        <img src={photo.photo} alt={photo.description} className="photo__image" />
                         <h3>{photo.photographer}</h3>
+                        <img src={photo.photo} alt={photo.description} className="photo__image" />
                     </div>
                 )}
                 <img />
@@ -46,6 +84,60 @@ export default function PhotoPage() {
                         <p className="photo__date"></p>
                     </div>
                     <p className="photo__photographer">Photo by: </p>
+                </div>
+
+                <div className="add-comment">
+                    <form onSubmit={handleCommentSubmit}>
+                        <div>
+                            <label>
+                                Name:
+                                <input
+                                    type="text"
+                                    name="name"
+                                    value={newComment.name}
+                                    onChange={handleInputChange}
+                                    required
+                                />
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                Comment:
+                                <textarea
+                                    name="comment"
+                                    value={newComment.comment}
+                                    onChange={handleInputChange}
+                                    required
+                                />
+                            </label>
+                        </div>
+                        <button type="submit">Add Comment</button>
+                    </form>
+                </div>
+
+
+                <div className="photo__comments">
+                    <div className="photo__comments">
+                        {comments.length > 0 ? (
+                            comments.map((comment) => (
+                                <div key={comment.id}>
+                                    <h4>{comment.name} <span>{new Date(comment.timestamp).toLocaleDateString('en-US')}</span></h4>
+                                    <p>{comment.comment}</p>
+                                </div>
+                            ))
+                        ) : (
+                            <p>No comments available.</p>
+                        )}
+                    </div>
+                    {/* {comment && (
+                        <div>
+                            <h2>Comments</h2>
+                            <p className="photo__comment">{comment.comment}</p>
+                        </div>
+                    )} */}
+                    <p className="photo__comment">  </p>
+                    <p className="photo__comment"></p>
+                    <p className="photo__comment"></p>
                 </div>
             </div>
 
