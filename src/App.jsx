d49371b3dc9b4661b45347840511b9ff51c14401
Chandni Melwani <cmelwani92@gmail.com>
2025-01-26 18:33:34 -0500

    added condition logic to conditionally render the hero componenet and not show it on photo page

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,18 @@ import MainPage from "./componenets/pages/MainPage/MainPage"
 import PhotoPage from './componenets/pages/PhotoPage/PhotoPage';
 import Hero from "./componenets/Hero/Hero"
 import Footer from './componenets/Footer/Footer';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 
 function App() {
 
   const [filterOpen, setFilterOpen] = useState(false);
 
+  const isPhotoPage = location.pathname.startsWith('/photo');
+
   return (
     <BrowserRouter>
     <Header filterOpen={filterOpen} setFilterOpen={setFilterOpen} />
-    <Hero />
-
+    {!isPhotoPage && <Hero />}
     <Routes>
       <Route path="/" element={ <MainPage filterOpen={filterOpen} />} />
       <Route path="/photo" element={ <PhotoPage />} />
