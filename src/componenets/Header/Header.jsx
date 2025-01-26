d01cb7e87568942cf65533bca66b0580c1734260
Chandni Melwani <cmelwani92@gmail.com>
2025-01-26 18:39:01 -0500

    replaced filter button for home button on header in photo page

--- src/componenets/Header/Header.jsx
+++ src/componenets/Header/Header.jsx
@@ -1,17 +1,26 @@
 import './Header.scss'
 import filterIcon from '../../assets/images/Filter.svg'
+import { useLocation, Link } from 'react-router-dom';
 
 function Header ({ filterOpen, setFilterOpen }) {
+
+    const isPhotoPage = useLocation().pathname.startsWith('/photo');
+
     return (
         <header className="header">
             <span className="header__title">Snaps</span>
-            <span>{filterOpen.toString()}</span>
-            <div 
-                className={`header__filter-button ${filterOpen ? "header__filter-button--active" : ""}`}
-                onClick={() => setFilterOpen((prev) => !prev)}
-            >Filters
-                <img src={filterIcon} alt="filter menu"/>
-            </div>
+            {isPhotoPage ? (
+                <Link to="/" className="header__home-button">
+                    Home
+                </Link>
+            ) : ( 
+                <div 
+                    className={`header__filter-button ${filterOpen ? "header__filter-button--active" : ""}`}
+                    onClick={() => setFilterOpen((prev) => !prev)}
+                > Filters
+                    <img src={filterIcon} alt="filter menu"/>
+                </div>
+            )}
         </header>
     );
 }
