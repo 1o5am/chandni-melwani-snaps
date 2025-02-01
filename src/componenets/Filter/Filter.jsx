bc7e1a30a2e81755456c5dd1587c3628c23726a7
Chandni Melwani <cmelwani92@gmail.com>
2025-02-01 01:23:11 -0500

    modified filters componenet to take data from api

--- src/componenets/Filter/Filter.jsx
+++ src/componenets/Filter/Filter.jsx
@@ -1,7 +1,13 @@
 import './Filter.scss'
 import tags from '../../data/tags.json'
+// get data from api
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams, useLocation } from 'react-router-dom'; 
 
 export default function Filter({ activeFilter, setActiveFilter }) {
+
+    const [tags, setTags] = useState([]);
 
     const handleFilterClick = (tag) => {
         console.log(`${tag} clicked!!`);
@@ -12,23 +18,58 @@ export default function Filter({ activeFilter, setActiveFilter }) {
         }
     };
 
+    useEffect(() => {
+        const fetchTags = async () => {
+            try {
+                const response = await axios.get('https://unit-3-project-c5faaab51857.herokuapp.com/tags?api_key=3311a5bf-592d-4c57-8cab-0478f94be322');
+                setTags(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        fetchTags();
+    }, []);
+
     return (
         <div className="filter">
             <div className="filter-text">Filters</div>
             <div className="filter-list">
-                {tags.map((tag, index) => {
-                    return (
-                        <span 
-                            key={index}
-                            className={`filter-list__tags ${activeFilter === tag ? "filter-list__tags--active" : ""}`}
-                            onClick={() => handleFilterClick(tag)}
-                        >
-                            {tag}
-                        </span>
-                    );
-                }
-            )}
+                {tags && (
+                    tags.map((tag, index) => {
+                        return (
+                            <span 
+                                key={index}
+                                className={`filter-list__tags ${activeFilter === tag ? "filter-list__tags--active" : ""}`}
+                                onClick={() => handleFilterClick(tag)}
+                            >
+                                {tag}
+                            </span>
+                        );
+                    })
+                )}
             </div>
-        </div>
-    );
+        </div>      
+    )    
 }
+
+
+//     return (
+//         <div className="filter">
+//             <div className="filter-text">Filters</div>
+//             <div className="filter-list">
+//                 {tags.map((tag, index) => {
+//                     return (
+//                         <span 
+//                             key={index}
+//                             className={`filter-list__tags ${activeFilter === tag ? "filter-list__tags--active" : ""}`}
+//                             onClick={() => handleFilterClick(tag)}
+//                         >
+//                             {tag}
+//                         </span>
+//                     );
+//                 }
+//             )}
+//             </div>
+//         </div>
+//     );
+// }
